e69073fc79567ef39308e26eb290405a1da12747
Murtadha Saad <ithasfinished@gmail.com>
2020-03-15 21:39:32 +0300

    fix bug related to scheme constants class import

--- samples/kotlin-dagger/src/main/java/co/windly/ktxaccount/sample/kotlindagger/utility/account/User.kt
+++ samples/kotlin-dagger/src/main/java/co/windly/ktxaccount/sample/kotlindagger/utility/account/User.kt
@@ -10,7 +10,7 @@ import co.windly.ktxaccount.annotation.DefaultLong
 import co.windly.ktxaccount.annotation.DefaultString
 
 @AccountScheme(mode = Mode.SINGLE)
-class AccountDefinition(
+class User(
 
   //region Id
 
--- samples/kotlin-dagger/src/main/java/co/windly/ktxaccount/sample/kotlindagger/utility/manager/AccountUtilityManager.kt
+++ samples/kotlin-dagger/src/main/java/co/windly/ktxaccount/sample/kotlindagger/utility/manager/AccountUtilityManager.kt
@@ -1,7 +1,7 @@
 package co.windly.ktxaccount.sample.kotlindagger.utility.manager
 
 import android.content.Context
-import co.windly.ktxaccount.sample.kotlindagger.utility.account.AccountDefinitionScheme
+import co.windly.ktxaccount.sample.kotlindagger.utility.account.UserScheme
 import io.reactivex.annotations.SchedulerSupport
 import io.reactivex.annotations.SchedulerSupport.IO
 import javax.inject.Inject
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 @Singleton
 @SchedulerSupport(value = IO)
 class AccountUtilityManager @Inject constructor(context: Context) :
-  AccountDefinitionScheme(context) {
+  UserScheme(context) {
 
   //region Authenticator
 
